dac8d92b1c27d9333f66e9e98780852351255a5c
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-03-18 12:59:49 +0100

    branch create and ppo to correct

--- MineSweeper.py
+++ MineSweeper.py
@@ -15,9 +15,6 @@ class MineSweeper(tk.Tk):
         self.start_time = None
         self.timer_label = tk.Label(self, text="Time: 0")
         self.timer_label.grid(row=height, columnspan=width)
-        self.colors = {"blue": "#0000FF", "green": "#008000", "red": "#FF0000",
-                       "purple": "#800080", "maroon": "#800000", "turquoise": "#40E0D0",
-                       "black": "#000000", "gray": "#808080"}
         self.create_widgets()
         self.place_mines()
         self.update_display()
@@ -58,7 +55,7 @@ class MineSweeper(tk.Tk):
                 self.game_over()
             else:
                 self.reveal_tile(x, y)
-    
+
     def reveal_tile(self, x, y):
         if self.tiles[y][x]["state"] == tk.NORMAL:
             value = self.board[y][x]
@@ -70,20 +67,7 @@ class MineSweeper(tk.Tk):
                         if 0 <= nx < self.width and 0 <= ny < self.height:
                             self.reveal_tile(nx, ny)
             else:
-                self.tiles[y][x].config(state=tk.DISABLED)
-
-                # Changer la couleur du texte après le clic
-                current_text = self.tiles[y][x].cget("text")
-                self.change_text_color(value, x, y, current_text)
-
-    def change_text_color(self, value, x, y, current_text):
-        color = self.colors.get(value)
-        if color:
-            self.tiles[y][x].config(text='<font color="{}">{}</font>'.format(color, current_text), state=tk.DISABLED)
-
-
-    
-
+                self.tiles[y][x].config(text=str(value), state=tk.DISABLED)
 
     def toggle_flag(self, event):
         x, y = self.winfo_pointerxy()
@@ -131,3 +115,16 @@ class MineSweeper(tk.Tk):
         new_game.mainloop()
 
 
+if __name__ == "__main__":
+    difficulty_levels = {
+        "Easy": {"width": 8, "height": 8, "mines_range": (10, 15)},
+        "Medium": {"width": 10, "height": 10, "mines_range": (20, 30)},
+        "Hard": {"width": 12, "height": 12, "mines_range": (30, 40)}
+    }
+    chosen_difficulty = "Medium"  # Change difficulty here
+    width = difficulty_levels[chosen_difficulty]["width"]
+    height = difficulty_levels[chosen_difficulty]["height"]
+    min_mines, max_mines = difficulty_levels[chosen_difficulty]["mines_range"]
+    num_mines = random.randint(min_mines, max_mines)
+    game = MineSweeper(width=width, height=height, num_mines=num_mines, difficulty=chosen_difficulty)
+    game.mainloop()
