f0794b5e715ad6b35d968089a472fbeddb71a845
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-03-19 13:55:24 +0100

    method added for begin scripts

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from MineSweeper import MineSweeper
 import random
 
-if __name__ == "__main__":
-    difficulty_levels = {
+def start_game():
+        difficulty_levels = {
         "Easy": {"width": 8, "height": 8, "mines_range": (10, 15)},
         "Medium": {"width": 10, "height": 10, "mines_range": (20, 30)},
         "Hard": {"width": 12, "height": 12, "mines_range": (30, 40)}
-    }
+        }
 
-    chosen_difficulty = "Medium"
-    width = difficulty_levels[chosen_difficulty]["width"]
-    height = difficulty_levels[chosen_difficulty]["height"]
-    min_mines, max_mines = difficulty_levels[chosen_difficulty]["mines_range"]
-    num_mines = random.randint(min_mines, max_mines)
-    game = MineSweeper(width=width, height=height, num_mines=num_mines, difficulty=chosen_difficulty)
-    game.mainloop()
+        chosen_difficulty = "Medium"
+        width = difficulty_levels[chosen_difficulty]["width"]
+        height = difficulty_levels[chosen_difficulty]["height"]
+        min_mines, max_mines = difficulty_levels[chosen_difficulty]["mines_range"]
+        num_mines = random.randint(min_mines, max_mines)
+        game = MineSweeper(width=width, height=height, num_mines=num_mines, difficulty=chosen_difficulty)
+        game.mainloop()
+        
+start_game()
+
+    
